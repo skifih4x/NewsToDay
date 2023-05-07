2f6670208a1f49b479419750ce4345044548bb79
Artem <artem-orlov92@bk.ru>
2023-05-07 15:34:13 +0300

    Initial Commit

---
+++ NewsToDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsToDay
+//
+//  Created by Артем Орлов on 07.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
