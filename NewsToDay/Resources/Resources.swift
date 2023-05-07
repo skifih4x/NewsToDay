d459fe5b74aa67af19eb25878818595936260d82
AndreyMikhailov <mands315@gmail.com>
2023-05-07 23:02:04 +0300

    Add Resources enum and UIColor extension

---
+++ NewsToDay/Resources/Resources.swift
@@ -0,0 +1,18 @@
+//
+//  Resource.swift
+//  NewsToDay
+//
+//  Created by Andrey on 07.05.2023.
+//
+
+import Foundation
+import UIKit
+
+enum Resources {
+    enum Colors {
+        
+    }
+    enum Icons {
+        
+    }
+}
