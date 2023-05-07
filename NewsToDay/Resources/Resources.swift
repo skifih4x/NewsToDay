8cdd624485da332daaab0c4fffcceaedd6a6ff39
AndreyMikhailov <mands315@gmail.com>
2023-05-08 00:21:31 +0300

    TabBar controller added

--- NewsToDay/Resources/Resources.swift
+++ NewsToDay/Resources/Resources.swift
@@ -10,9 +10,17 @@ import UIKit
 
 enum Resources {
     enum Colors {
-        
+        static var active = UIColor(hex: 0x475AD7)
+        static var inactive = UIColor(hex: 0xACAFC3)
+        static var separator = UIColor(hex: 0xACAFC352)
     }
+    
     enum Icons {
-        
+        enum TabBar {
+            static var home = UIImage(named: "tabBarHome.svg")
+            static var categories = UIImage(named: "tabBarApps.svg")
+            static var bookmarks = UIImage(named: "tabBarBookmark.svg")
+            static var profile = UIImage(named: "tabBarUser.svg")
+        }
     }
 }
