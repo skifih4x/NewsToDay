8c6c17fb9ba35189bb1d527b7d83bd271ddb5a8a
Artem <artem-orlov92@bk.ru>
2023-05-07 22:11:04 +0300

    add new folder

--- NewsToDay/Controllers/HomeVC/HomeViewController.swift
+++ NewsToDay/Controllers/HomeVC/HomeViewController.swift
@@ -11,9 +11,5 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
