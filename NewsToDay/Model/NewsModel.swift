338fa9945df1a398611996100901a7d20566e6d3
Artem <artem-orlov92@bk.ru>
2023-05-08 20:36:23 +0300

    add model for network

--- NewsToDay/Model/NewsModel.swift
+++ NewsToDay/Model/NewsModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct Welcome: Codable {
+    let status: String
+    let totalResults: Int
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let author: String?
+    let title, description: String
+    let url: String
+    let urlToImage: String?
+    let publishedAt: String
+    let content: String
+}
+
+struct Source: Codable {
+    let id: ID?
+    let name: String
+}
+
+enum ID: String, Codable {
+    case businessInsider = "business-insider"
+    case dieZeit = "die-zeit"
+    case engadget = "engadget"
+    case googleNews = "google-news"
+    case wired = "wired"
+}
