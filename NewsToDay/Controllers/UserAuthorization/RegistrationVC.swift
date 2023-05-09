b5457bf0f31fe608e0469f4be21eb8583daf0ca9
AndreyMikhailov <mands315@gmail.com>
2023-05-09 11:21:36 +0300

    Add Registration VC and View

--- NewsToDay/Controllers/UserAuthorization/RegistrationVC.swift
+++ NewsToDay/Controllers/UserAuthorization/RegistrationVC.swift
@@ -13,10 +13,46 @@ class RegistrationViewController: CustomViewController<RegistrationView> {
         super.viewDidLoad()
         
         customView.delegate = self
+        customView.userNameTextField.delegate = self
+        customView.emailTextField.delegate = self
+        customView.passwordTextField.delegate = self
+        customView.confirmPasswordTextField.delegate = self
     }
 }
 
 extension RegistrationViewController: RegistrationViewDelegate {
-    func RegistrationView(_ view: RegistrationView, didTapButton button: UIButton) {
+    func RegistrationView(_ view: RegistrationView, didTapSignUpButton button: UIButton) {
+        
+    }
+    
+    func RegistrationView(_ view: RegistrationView, didTapOpenSignInButton button: UIButton) {
+        
+    }
+}
+
+extension RegistrationViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let currentText = textField.text ?? ""
+        let newText = (currentText as NSString).replacingCharacters(in: range, with: string)
+        let hasText = !newText.isEmpty
+        
+        if hasText {
+            if let containerView = textField.leftView {
+                if let imageView = containerView.subviews.first as? UIImageView {
+                    imageView.tintColor = Resources.Colors.purplePrimary
+                }
+            }
+        }
+        else {
+            if let containerView = textField.leftView {
+                if let imageView = containerView.subviews.first as? UIImageView {
+                    imageView.tintColor = Resources.Colors.greyPrimary
+                }
+            }
+        }
+
+        return true
     }
 }
