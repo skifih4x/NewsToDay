6db5dc5cd1c8aff2b4d48b9799ae268a19889bf9
AndreyMikhailov <mands315@gmail.com>
2023-05-16 17:28:54 +0300

    Set height for row

--- NewsToDay/Controllers/BookmarksVC/BookMarksViewController.swift
+++ NewsToDay/Controllers/BookmarksVC/BookMarksViewController.swift
@@ -154,6 +154,10 @@ class BookmarksViewController: UIViewController, UITableViewDelegate, UITableVie
 
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 }
     
     extension BookmarksViewController {
