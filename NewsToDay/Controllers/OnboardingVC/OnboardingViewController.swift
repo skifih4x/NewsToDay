140c92d698ef773ab57043f538d4ff61499b422d
Anton Medovnik <krimeainbox@yandex.ru>
2023-05-21 13:29:26 +0300

    Onboarding changes

--- NewsToDay/Controllers/OnboardingVC/OnboardingViewController.swift
+++ NewsToDay/Controllers/OnboardingVC/OnboardingViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  OnboardingViewController.swift
 //  NewsToDay
@@ -9,17 +10,16 @@ import Foundation
 import UIKit
 
 class OnboardingViewController: UIViewController    {
-    
-    private let localizationManager = LocalizationManager.localizationManager
-    
+
+
     let stackView = UIStackView ()
     let button = UIButton (type: .system)
     let allNewsLabel = UILabel()
-    let firstToKnowLabel = UILabel ()
+    let firstToNowLabel = UILabel ()
     let pageControl = UIPageControl()
     let scrollView = UIScrollView()
-    let imagesOfCity = ["mainScreen", "categories"]
-    
+    let imagesOfCity = ["mainScreen", "catigory"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
@@ -30,39 +30,45 @@ class OnboardingViewController: UIViewController    {
         stackViewConfigure()
         constrainrs()
         button.addTarget(self, action: #selector(didTapButton(_ :)), for: .touchUpInside)
+      
+      
     }
-    
+  
     //Scroll View
     func scrollViewConfigure () {
         stackView.addSubview(scrollView)
         scrollView.contentSize = CGSize(width: view.frame.size.width*2, height: scrollView.frame.size.width)
         
-        //        scrollView.isPagingEnabled = true
+//        scrollView.isPagingEnabled = true
         
         for x in 0..<2 {
             let xPos = CGFloat(x)*self.view.frame.size.width + 60
-            let page = UIImageView(frame: CGRect(x: xPos / 1.2, y: 20, width: 286, height: 336))
-            
-            scrollView.contentSize = CGSize(width: 720, height: scrollView.frame.size.height)
+                let page = UIImageView(frame: CGRect(x: xPos / 1.2, y: 20, width: 286, height: 336))
+              
+                scrollView.contentSize = CGSize(width: 720, height: scrollView.frame.size.height)
             
             page.image = UIImage(named: imagesOfCity[x])
-            
-            pageControl.currentPage += 1
+    
+          pageControl.currentPage += 1
             page.layer.cornerRadius = 20
             scrollView.addSubview(page)
+            
+        
         }
-    }
-    
+        
+       
+        }
+        
     @objc func didTapButton (_ button: UIButton) {
         guard button.tag < 1 else {
             dismiss(animated: true, completion: nil)
             return
         }
         pageControl.currentPage += 1
-        //        firstToKnowLabel.text = "Все новости в одном месте"
+//        firstToNowLabel.text = "Все новости в одном месте"
         scrollView.setContentOffset(CGPoint(x:  view.frame.size.width - 70, y: 0), animated: true)
-        button.setTitle(localizationManager.localizeString("ONBOARDING_GET_STARTED_BUTTON"), for: .normal)
-        
+        button.setTitle(NSLocalizedString("ONBOARDING_GET_STARTED_BUTTON", comment: "Get Started"), for: .normal)
+       
         if button.titleLabel?.text == "Get Started" || button.titleLabel?.text == "Начать" {
             UserDefaults.standard.set(true, forKey: "launchedBefore")
             let authVC = AuthorizationViewController()
@@ -72,49 +78,56 @@ class OnboardingViewController: UIViewController    {
     }
     
     //StackView
+    
     func stackViewConfigure () {
         view.addSubview(stackView)
         stackView.backgroundColor = .white
         stackView.addSubview(button)
     }
     
+
     //Button
-    func buttonConfigure () {
-        button.configure(title: localizationManager.localizeString("ONBOARDING_NEXT_BUTTON"))
-    }
     
+    func buttonConfigure () {
+        button.configure(title: NSLocalizedString("ONBOARDING_NEXT_BUTTON", comment: "Next"))
+    }
     // Labels
+    
     func labelConfigure () {
         stackView.addSubview(allNewsLabel)
-        stackView.addSubview(firstToKnowLabel)
+        stackView.addSubview(firstToNowLabel)
         allNewsLabel.numberOfLines = 2
         allNewsLabel.textColor = .gray
         allNewsLabel.textAlignment = .center
-        allNewsLabel.text = localizationManager.localizeString("ONBOARDING_FIRST_COMMENT_LABEL")
-        firstToKnowLabel.text = localizationManager.localizeString("ONBOARDING_FIRST_LABEL")
-        firstToKnowLabel.textAlignment = .center
-        firstToKnowLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        allNewsLabel.text = NSLocalizedString("ONBOARDING_ALL_NEWS_LABEL", comment: "All news in one place, be \nthe first to know last news")
+        firstToNowLabel.text = NSLocalizedString("ONBOARDING_FIRST_TO_KNOW_LABEL", comment: "First to know")
+        firstToNowLabel.textAlignment = .center
+        firstToNowLabel.font = UIFont.boldSystemFont(ofSize: 25)
     }
     
     //PageControl
+    
     func pageControlFunc () {
         stackView.addSubview(pageControl)
         pageControl.isEnabled = false
         pageControl.numberOfPages = imagesOfCity.count
         pageControl.tintColor = UIColor(named: "purpleColor")
-        
+     
         pageControl.pageIndicatorTintColor = UIColor(named: "grayLighter")
         pageControl.currentPageIndicatorTintColor = UIColor(named: "purpleColor")
     }
-        
+    
+    
     //Constraints
+    
     func constrainrs () {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         button.translatesAutoresizingMaskIntoConstraints = false
         allNewsLabel.translatesAutoresizingMaskIntoConstraints = false
-        firstToKnowLabel.translatesAutoresizingMaskIntoConstraints = false
+        firstToNowLabel.translatesAutoresizingMaskIntoConstraints = false
         pageControl.translatesAutoresizingMaskIntoConstraints = false
         scrollView.translatesAutoresizingMaskIntoConstraints = false
+       
         
         NSLayoutConstraint.activate([
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -129,35 +142,41 @@ class OnboardingViewController: UIViewController    {
             allNewsLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -20),
             allNewsLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 20),
             allNewsLabel.bottomAnchor.constraint(equalTo: stackView.safeAreaLayoutGuide.bottomAnchor, constant: -170),
-            
-            firstToKnowLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -20),
-            firstToKnowLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 20),
-            firstToKnowLabel.bottomAnchor.constraint(equalTo: stackView.safeAreaLayoutGuide.bottomAnchor, constant: -242),
+
+            firstToNowLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -20),
+            firstToNowLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 20),
+            firstToNowLabel.bottomAnchor.constraint(equalTo: stackView.safeAreaLayoutGuide.bottomAnchor, constant: -242),
             
             pageControl.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: -20),
             pageControl.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 20),
             pageControl.bottomAnchor.constraint(equalTo: stackView.safeAreaLayoutGuide.bottomAnchor, constant: -356),
-            //
+//
             scrollView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0),
             scrollView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0),
             scrollView.topAnchor.constraint(equalTo: stackView.topAnchor, constant: 50),
             scrollView.bottomAnchor.constraint(equalTo: pageControl.topAnchor, constant: -30),
+
+//            pager.trailingAnchor.constraint(equalTo: viewOfImages.trailingAnchor, constant: 0),
+//            pager.leadingAnchor.constraint(equalTo: viewOfImages.leadingAnchor, constant: 0),
+//            pager.heightAnchor.constraint(equalToConstant: 200)
+//
             
-            //            pager.trailingAnchor.constraint(equalTo: viewOfImages.trailingAnchor, constant: 0),
-            //            pager.leadingAnchor.constraint(equalTo: viewOfImages.leadingAnchor, constant: 0),
-            //            pager.heightAnchor.constraint(equalToConstant: 200)
+            
         ])
     }
 }
 
 extension OnboardingViewController : UIScrollViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        pageControl.currentPage = Int(floorf(Float(scrollView.contentOffset.x)) - Float (70))
-        firstToKnowLabel.text = localizationManager.localizeString("ONBOARDING_SECOND_LABEL")
-        allNewsLabel.text = localizationManager.localizeString("ONBOARDING_SECOND_COMMENT_LABEL")
-        if pageControl.currentPage == 0 {
-            firstToKnowLabel.text = localizationManager.localizeString("ONBOARDING_FIRST_LABEL")
-            allNewsLabel.text = localizationManager.localizeString("ONBOARDING_FIRST_COMMENT_LABEL")
-        }
+     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+         pageControl.currentPage = Int(floorf(Float(scrollView.contentOffset.x)) - Float (70))
+         firstToNowLabel.text = "Категории"
+         allNewsLabel.text = NSLocalizedString("ONBOARDING_ALL_NEWS_LABEL_TWO", comment: "The latest news from Russia \nand around the world")
+         if pageControl.currentPage == 0 {
+             firstToNowLabel.text = "Главный экран"
+             allNewsLabel.text = NSLocalizedString("ONBOARDING_ALL_NEWS_LABEL", comment: "All news in one place, be \nthe first to know last news")
+             
+         }
+        
     }
 }
+
